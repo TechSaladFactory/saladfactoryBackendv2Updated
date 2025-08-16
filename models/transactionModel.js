cb140e800ba771c0df8e2c63c256ec6ff93dead1
TechSaladFactory <techsaladfactory@gmail.com>
2025-08-16 23:59:46 +0300

    Update transactionModel.js

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -4,60 +4,41 @@ const TransactionSchema = new mongoose.Schema({
   productID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
-    required: [false, "Product ID is required"]
-  },
-  product_name: {
-    type: String,
-    trim: true
+    required: false // في INEXIST ممكن يكون فاضي
   },
   type: {
     type: String,
-    enum: ["IN", "OUT"],
-    required: [false, "Transaction type is required"]
+    enum: ["IN", "OUT", "INEXIST"], // ✅ أضفنا INEXIST
+    required: [true, "Transaction type is required"]
   },
   quantity: {
     type: Number,
-    default: 0,
-
-    required: [false, "Quantity is required"],
-    min: [1, "Quantity must be at least 1"]
   },
-  unit: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Unit"
+  price: {
+    type: Number,
   },
-  department: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Department"
+  expiredDate: {
+    type: String,
   },
   supplier: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Supplier"
-
   },
   userID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: [false, "User ID is required"]
+    required: [true, "User ID is required"]
   },
-  user_name: {
+  packSize: {
     type: String,
     trim: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  packSize:{
-    type:String,
   },
   note: {
     type: String,
     trim: true
-  },
-
+  }
 }, {
   timestamps: true
 });
 
-exports.TransactionModel = mongoose.model("Transaction", TransactionSchema)
+exports.TransactionModel = mongoose.model("Transaction", TransactionSchema);

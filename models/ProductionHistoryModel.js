85b2ded47b4735d51379ddb65ad314eca9a4ecec
TechSaladFactory <techsaladfactory@gmail.com>
2025-08-22 19:01:44 +0300

    Create ProductionHistoryModel.js

---
+++ models/ProductionHistoryModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productionHistorySchema = new mongoose.Schema({
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ProductOP",
+        required: true,
+      },
+      qty: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    },
+  ],
+  action: {
+    type: String,
+    default: "create", // create أو update
+  },
+}, { timestamps: true });
+
+exports.ProductionHistoryModel = mongoose.model("ProductionHistory", productionHistorySchema);

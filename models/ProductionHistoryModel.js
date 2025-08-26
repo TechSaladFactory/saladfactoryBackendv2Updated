b16a98f5772c37803cf6109ca195f939fa64dd55
TechSaladFactory <techsaladfactory@gmail.com>
2025-08-26 15:38:50 +0300

    Update ProductionHistoryModel.js

--- models/ProductionHistoryModel.js
+++ models/ProductionHistoryModel.js
@@ -1,40 +1,30 @@
 const mongoose = require("mongoose");
 
-const productionHistorySchema = new mongoose.Schema(
-  {
-    items: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "ProductOP",
-          required: false, // 👈 مش إجباري
-        },
-        qty: {
-          type: Number,
-          default: 1, // 👈 قيمة افتراضية
-          min: 0,
-          required: false, // 👈 مش إجباري
-        },
+const productionHistorySchema = new mongoose.Schema({
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ProductOP",
+        required: true,
       },
-    ],
-    branch: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Branch",
-      required: false, // 👈 مش إجباري
+      qty: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+   
     },
-    action: {
-      type: String,
-      default: "create", // create أو update
-    },
-    note: {
-      type: String,
-      required: false, // 👈 إضافة note كـ optional
-    },
+  ],
+  branch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Branch",
+    required: false,
   },
-  { timestamps: true }
-);
+  action: {
+    type: String,
+    default: "create", // create أو update
+  },
+}, { timestamps: true });
 
-exports.ProductionHistoryModel = mongoose.model(
-  "ProductionHistory",
-  productionHistorySchema
-);
+exports.ProductionHistoryModel = mongoose.model("ProductionHistory", productionHistorySchema);

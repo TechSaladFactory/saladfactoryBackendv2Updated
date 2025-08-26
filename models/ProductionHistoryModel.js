d88fb261df28d2d0ebef4398d69c31526232b0ba
TechSaladFactory <techsaladfactory@gmail.com>
2025-08-26 15:18:04 +0300

    Update ProductionHistoryModel.js

--- models/ProductionHistoryModel.js
+++ models/ProductionHistoryModel.js
@@ -19,7 +19,7 @@ const productionHistorySchema = new mongoose.Schema({
   branch: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Branch",
-    required: false,
+    required: true,
   },
   action: {
     type: String,
